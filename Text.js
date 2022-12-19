cd6fa912530ff4b26e83893d9b55e48eaa3fbac7
Taisuke Fukuno <fukuno@jig.jp>
2022-12-19 16:52:32 +0900

    複数の電子署名に対応

--- Text.js
+++ Text.js
@@ -16,9 +16,14 @@ export class Text {
     const s2 = s.trim().split("\n");
     let ss = s2.map(s => s.trim());
     if (ss.length > 0 && ss[ss.length - 1].startsWith(SIGN_PREFIX)) {
-      return ss[ss.length - 1].substring(SIGN_PREFIX.length);
+      const sign = ss[ss.length - 1].substring(SIGN_PREFIX.length);
+      const ss2 = [];
+      for (let i = 0; i < ss.length - 1; i++) {
+        ss2.push(ss[i]);
+      }
+      return [ss2.join("\n"), sign];
     }
-    return null;
+    return [s, null];
   }
   static addSign(s, sign) {
     return s + "\n\n" + SIGN_PREFIX + sign;
